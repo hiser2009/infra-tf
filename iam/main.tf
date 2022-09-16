9c158ad802ce97adbc353c9f90575573208288de
Ronny Hise <hiser@Ronnys-MBP.home>
2022-09-16 00:26:59 -0700

    Add Module TF.

---
+++ iam/main.tf
@@ -0,0 +1,8 @@
+module "iam_account" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-account"
+
+  account_alias = var.account_alias
+
+  minimum_password_length = var.minimum_password_length
+  require_numbers         = var.require_numbers
+}
